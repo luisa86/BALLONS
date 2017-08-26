ada576eeba8bdabfb0bea6d9d67bd3b6cdf4ccaa
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-08-26 18:19:25 +0200

    Made Object Questions

---
+++ starter-code/js/ballons.js
@@ -0,0 +1,194 @@
+
+var questions = {
+    assigantures : [
+      {
+        math : [
+          {
+            question : "[(8+5)-3)*2+4]/10",
+            answer : [
+              {option: "2", correct: "false"},
+              {option: "2.4", correct: "false"},
+              {option: "6", correct: "true"},
+              {option: "1.8", correct: "false"},
+            ]
+          },
+
+          {
+            question : "3(x-4)=18.¿Cuál es el valor de X?",
+            answer : [
+              {option: "6", correct: "false"},
+              {option: "22/3", correct: "false"},
+              {option: "14/3", correct: "false"},
+              {option: "10", correct: "true"},
+            ]
+          },
+
+
+          {
+            question : "4 x 4 - 4 + 4 x 4 = ¿...? ",
+            answer : [
+              {option: "-4", correct: "false"},
+              {option: "28", correct: "true"},
+              {option: "64", correct: "false"},
+              {option: "-16", correct: "false"},
+            ]
+          },
+
+
+          {
+            question : "Si x+3= y, ¿cuánto es 2x + 6?",
+            answer : [
+              {option: "y", correct: "false"},
+              {option: "no se puede terminar", correct: "false"},
+              {option: "2y", correct: "true"},
+              {option: "4y", correct: "false"},
+            ]
+          },
+
+]
+},
+{
+
+    english : [
+      {
+        question : "Are you and your friends German?",
+        answer : [
+          {option: "Yes, I am.", correct: "false"},
+          {option: "Yes, we are.", correct: "true"},
+          {option: "Yes, they are.", correct: "false"},
+          {option: "Yes, I do", correct: "false"},
+        ]
+      },
+
+      {
+        question : "There's John and ____ wife.",
+        answer : [
+          {option: "His", correct: "true"},
+          {option: "He´s", correct: "false"},
+          {option: "Her", correct: "false"},
+          {option: "Hi´s", correct: "false"},
+        ]
+      },
+
+
+      {
+        question : "They are going to the UK with ____ son.",
+        answer : [
+          {option: "Thre´re", correct: "false"},
+          {option: "They", correct: "false"},
+          {option: "There", correct: "false"},
+          {option: "Their", correct: "true"},
+        ]
+      },
+
+
+      {
+        question : "That computer ____ in Thailand.",
+        answer : [
+          {option: "Were made.", correct: "false"},
+          {option: "Was make.", correct: "false"},
+          {option: "Was made.", correct: "true"},
+          {option: "Were make", correct: "false"},
+        ]
+      },
+
+]
+},
+
+{
+
+  sciencie : [
+    {
+      question : "What is the minimum component that forms living beings?",
+      answer : [
+        {option: "Particle", correct: "false"},
+        {option: "Tissue", correct: "false"},
+        {option: "Atoms", correct: "false"},
+        {option: "Cell", correct: "true"},
+      ]
+    },
+
+    {
+      question : "One milligram is:",
+      answer : [
+        {option: "Are 0.001 grams", correct: "false"},
+        {option: "Both are true", correct: "true"},
+        {option: "It's a thousand micrograms", correct: "false"},
+        {option: "Both are false", correct: "false"},
+      ]
+    },
+
+
+    {
+      question : "The speed at which the light travels is:",
+      answer : [
+        {option: "30.000 km/h", correct: "false"},
+        {option: "300.000 km/s", correct: "true"},
+        {option: "300.000 m/s", correct: "false"},
+        {option: "30.000 mi/s", correct: "false"},
+      ]
+    },
+
+
+    {
+      question : "The column to the right of the periodic table is composed by:",
+      answer : [
+        {option: "Minerals", correct: "false"},
+        {option: "Noble gases", correct: "true"},
+        {option: "Alkaline", correct: "false"},
+        {option: "Halides", correct: "false"},
+      ]
+    },
+
+]
+},
+
+{
+
+  geography : [
+    {
+      question : "How many countries does Argentina limit?",
+      answer : [
+        {option: "3", correct: "false"},
+        {option: "4", correct: "false"},
+        {option: "6", correct: "false"},
+        {option: "5", correct: "true"},
+      ]
+    },
+
+    {
+      question : "What is the international code for Cuba ?.",
+      answer : [
+        {option: "CA", correct: "false"},
+        {option: "NO ANSWER IS CORRECT", correct: "false"},
+        {option: "CB", correct: "false"},
+        {option: "CU", correct: "true"},
+      ]
+    },
+
+
+    {
+      question : "What is the capital of Libya?",
+      answer : [
+        {option: "Trípoli", correct: "true"},
+        {option: "Beirut", correct: "false"},
+        {option: "Cairo", correct: "false"},
+        {option: "Riad", correct: "false"},
+      ]
+    },
+
+
+    {
+      question : "How many islets do the Canary Islands have?",
+      answer : [
+        {option: "7", correct: "false"},
+        {option: "6", correct: "true"},
+        {option: "5", correct: "false"},
+        {option: "4", correct: "false"},
+      ]
+    },
+
+  ]
+}
+  ]
+};
